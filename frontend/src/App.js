36f4ce4df310ed28041d243da2f9717da7b6fa5c
udaysa97 <uday.sa97@gmail.com>
2020-11-18 22:22:45 +0530

    react base added

---
+++ frontend/src/App.js
@@ -0,0 +1,171 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            flipzon
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign in</a>
+        </div>
+      </header>
+
+      <main>
+        <div className="row centre">
+          <div className="card">
+            <a href="produt.html">
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>brand x slim shirt</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">&#8377;120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="produt.html">
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>brand x slim shirt</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">&#8377;120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="produt.html">
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>brand x slim shirt</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">&#8377;120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="produt.html">
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>brand x slim shirt</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">&#8377;120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="produt.html">
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>brand x slim shirt</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star-half-o"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star-o"></i>
+                </span>
+              </div>
+              <div className="price">&#8377;120</div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="row centre">All Rights Reserved &copy;</footer>
+    </div>
+  );
+}
+
+export default App;
