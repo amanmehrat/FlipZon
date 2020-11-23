fa297676e51f0cc88f605983e0500550bb04b2c7
udaysa97 <uday.sa97@gmail.com>
2020-11-24 00:13:24 +0530

    sign in actions complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
 import CartScreen from "./Screens/CartScreen";
 
 import HomeScreen from "./Screens/HomeScreen";
@@ -11,6 +12,12 @@ import SigninScreen from "./Screens/SigninScreen";
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -25,7 +32,22 @@ function App() {
             {cartItems.length > 0 && (
               <span className="badge">{cartItems.length}</span>
             )}
-            <Link to="/signin">Sign in</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name + " "}
+                  <i className="fa fa-caret-down"></i>
+                  {"  "}
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign in</Link>
+            )}
           </div>
         </header>
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,26 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
+import { userSignInReducer } from "./reducers/userReducer";
 
-const initialState = {};
+const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSignInReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
