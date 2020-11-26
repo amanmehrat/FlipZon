04d4303b1ce1c5ba0f5fe1aa9f8f6f77f814c553
udaysa97 <uday.sa97@gmail.com>
2020-11-27 01:32:43 +0530

    PayPal payment method added

--- frontend/src/Screens/OrderScreen.js
+++ frontend/src/Screens/OrderScreen.js
@@ -1,19 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { PayPalButton } from "react-paypal-button-v2";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { viewOrder } from "../actions/orderActions";
+import Axios from "axios";
 
 export default function OrderScreen(props) {
   const orderId = props.match.params.id;
+  const [sdkReady, setSdkReady] = useState(false);
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, loading, error } = orderDetails;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(viewOrder(orderId));
-  }, [dispatch, orderId]); // order._id not used as order may be null and dont want errors
+    const addPayPalScript = async () => {
+      console.log("inside paypal function");
+      const { data } = await Axios.get("/api/config/paypal");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `https://www.paypal.com/sdk/js?client-id=${data}&currency=INR`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+    console.log("aaaaaaaaaaaaaaaaa" + order + orderId);
+    if (!order || !order._id) {
+      dispatch(viewOrder(orderId));
+    } else {
+      if (!order.isPaid) {
+        if (!window.paypal) {
+          addPayPalScript();
+        } else {
+          setSdkReady(true);
+        }
+      }
+    }
+  }, [dispatch, order, orderId, sdkReady]); // order._id not used as order may be null and dont want errors
+
+  const successPaymentHandler = () => {
+    // TODO dispatch pay order
+  };
+
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
@@ -126,7 +157,19 @@ export default function OrderScreen(props) {
                   </div>
                 </div>
               </li>
-              <li></li>
+              {!order.isPaid && (
+                <li>
+                  {!sdkReady ? (
+                    <LoadingBox></LoadingBox>
+                  ) : (
+                    <PayPalButton
+                      amount={order.totalPrice}
+                      currency="INR"
+                      onSuccess={successPaymentHandler}
+                    ></PayPalButton>
+                  )}
+                </li>
+              )}
             </ul>
           </div>
         </div>
